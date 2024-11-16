704ca38226eaba7061611aeca7635f1b615073d6
SakataGintoki45 <sakatagintoki89g@gmail.com>
2024-11-15 22:19:11 -0600

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,269 @@
+namespace sorting_methods
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            string input = txtInput.Text;
+            int[] numbers;
+
+            try
+            {
+                numbers = input.Split(',').Select(int.Parse).ToArray();
+            }
+            catch
+            {
+                MessageBox.Show("Please enter a list of valid numbers separated by commas.");
+                return;
+            }
+            switch (cmbSortMethod.SelectedItem.ToString())
+            {
+                case "Bubble Sort":
+                    BubbleSort(numbers);
+                    break;
+                case "Selection Sort":
+                    SelectionSort(numbers);
+                    break;
+                case "Insertion Sort":
+                    InsertionSort(numbers);
+                    break;
+                case "Quick Sort":
+                    QuickSort(numbers, 0, numbers.Length - 1);
+                    break;
+                case "Merge Sort":
+                    MergeSort(numbers, 0, numbers.Length - 1);
+                    break;
+                case "Heap Sort":
+                    HeapSort(numbers);
+                    break;
+                case "Radix Sort":
+                    RadixSort(numbers);
+                    break;
+                default:
+                    MessageBox.Show("Select a sorting method.");
+                    return;
+            }
+
+            txtSortedOutput.Text = string.Join(", ", numbers);
+        }
+
+        private void BubbleSort(int[] arr)
+        {
+            int temp;
+            bool swapped;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                swapped = false;
+                for (int j = 0; j < arr.Length - 1 - i; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                        swapped = true;
+                    }
+                }
+                if (!swapped)
+                    break;
+            }
+        }
+
+        private void SelectionSort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                int temp = arr[minIndex];
+                arr[minIndex] = arr[i];
+                arr[i] = temp;
+            }
+        }
+
+        private void InsertionSort(int[] arr)
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                int key = arr[i];
+                int j = i - 1;
+
+                while (j >= 0 && arr[j] > key)
+                {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                }
+
+                arr[j + 1] = key;
+            }
+        }
+
+        private void QuickSort(int[] arr, int low, int high)
+        {
+            if (low < high)
+            {
+                int pi = Partition(arr, low, high);
+
+                QuickSort(arr, low, pi - 1);
+                QuickSort(arr, pi + 1, high);
+            }
+        }
+        private int Partition(int[] arr, int low, int high)
+        {
+            int pivot = arr[high];
+            int i = (low - 1);
+
+            for (int j = low; j < high; j++)
+            {
+                if (arr[j] < pivot)
+                {
+                    i++;
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+
+            int swapTemp = arr[i + 1];
+            arr[i + 1] = arr[high];
+            arr[high] = swapTemp;
+
+            return i + 1;
+        }
+
+        private void MergeSort(int[] arr, int left, int right)
+        {
+            if (left < right)
+            {
+                int mid = (left + right) / 2;
+
+                MergeSort(arr, left, mid);
+                MergeSort(arr, mid + 1, right);
+
+                Merge(arr, left, mid, right);
+            }
+        }
+
+        private void Merge(int[] arr, int left, int mid, int right)
+        {
+            int n1 = mid - left + 1;
+            int n2 = right - mid;
+
+            int[] L = new int[n1];
+            int[] R = new int[n2];
+
+            Array.Copy(arr, left, L, 0, n1);
+            Array.Copy(arr, mid + 1, R, 0, n2);
+
+            int i = 0, j = 0, k = left;
+            while (i < n1 && j < n2)
+            {
+                if (L[i] <= R[j])
+                {
+                    arr[k] = L[i];
+                    i++;
+                }
+                else
+                {
+                    arr[k] = R[j];
+                    j++;
+                }
+                k++;
+            }
+
+            while (i < n1)
+            {
+                arr[k] = L[i];
+                i++;
+                k++;
+            }
+
+            while (j < n2)
+            {
+                arr[k] = R[j];
+                j++;
+                k++;
+            }
+        }
+
+        private void HeapSort(int[] arr)
+        {
+            int n = arr.Length;
+
+            for (int i = n / 2 - 1; i >= 0; i--)
+                Heapify(arr, n, i);
+
+            for (int i = n - 1; i >= 0; i--)
+            {
+                int temp = arr[0];
+                arr[0] = arr[i];
+                arr[i] = temp;
+
+                Heapify(arr, i, 0);
+            }
+        }
+
+        private void Heapify(int[] arr, int n, int i)
+        {
+            int largest = i;
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+
+            if (left < n && arr[left] > arr[largest])
+                largest = left;
+
+            if (right < n && arr[right] > arr[largest])
+                largest = right;
+
+            if (largest != i)
+            {
+                int swap = arr[i];
+                arr[i] = arr[largest];
+                arr[largest] = swap;
+
+                Heapify(arr, n, largest);
+            }
+        }
+
+        private void RadixSort(int[] arr)
+        {
+            int max = arr.Max();
+
+            for (int exp = 1; max / exp > 0; exp *= 10)
+                CountSort(arr, exp);
+        }
+
+        private void CountSort(int[] arr, int exp)
+        {
+            int[] output = new int[arr.Length];
+            int[] count = new int[10];
+
+            for (int i = 0; i < arr.Length; i++)
+                count[(arr[i] / exp) % 10]++;
+
+            for (int i = 1; i < 10; i++)
+                count[i] += count[i - 1];
+
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+                count[(arr[i] / exp) % 10]--;
+            }
+
+            Array.Copy(output, 0, arr, 0, arr.Length);
+        }
+
+    }
+}
